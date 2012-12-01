d931c8c80e028ac195aea1248d9695c833192f3a
Brendan Dixon <brendandixon@me.com>
2012-12-01 10:59:00 -0800

    - More UI fiddling

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.in_name_order.all
 
     respond_to do |format|
       format.html # index.html.erb
