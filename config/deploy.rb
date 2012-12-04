0d085d645b848066cb5c519be0636511cd9c4a00
Brendan Dixon <brendandixon@me.com>
2012-12-03 16:42:46 -0800

    - More work on Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,20 @@
 require 'bundler/capistrano'
 
+set :default_environment, {
+  'RBENV_ROOT' => '/usr/local/rbenv',
+  'PATH' => "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH"
+}
+set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
+set :use_sudo, false
+
 set :application, 'tracker'
 set :deploy_to, '/Users/dixonb/Sites/tracker'
+set :port, '8080'
 
 set :scm, :git
 set :repository, 'git@github.com:aws/tracker.git'
 set :git_shallow_clone, 1
 set :deploy_via, :remote_cache
-
 set :keep_releases, 5
 
 server 'localhost', :app, :web, :db, primary: true
@@ -15,10 +22,15 @@ server 'localhost', :app, :web, :db, primary: true
 after 'deploy:update_code', 'deploy:migrate'
 after "deploy:restart", "deploy:cleanup"
 
-# If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+  task :start do
+    run "bundle exec passenger start -d -p #{port}"
+  end
+  
+  task :stop do
+    run "bundle exec passenger stop -p #{port}"
+  end
+  
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
