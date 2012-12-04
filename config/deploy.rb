4ef22e616690002bee56ba94df4d6df13b082fbc
Brendan Dixon <brendandixon@me.com>
2012-12-03 19:59:25 -0800

    - Completed Capistrano deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :passenger_port, '8080'
 
 set :scm, :git
 set :repository, 'git@github.com:aws/tracker.git'
+# set :branch, 'origin/master'
 set :git_shallow_clone, 1
 set :deploy_via, :remote_cache
 set :keep_releases, 5
@@ -23,15 +24,39 @@ after 'deploy:update_code', 'deploy:migrate'
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
+  # task :default do
+  #   update
+  #   restart
+  #   cleanup
+  # end
+  # 
+  # desc "Setup a GitHub-style deployment."
+  # task :setup, except: {no_release: true} do
+  #   run "git clone #{repository} #{current_path}"
+  # end
+  # 
+  # desc "Update the deployed code."
+  # task :update_code, except: {no_release: true} do
+  #   run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+  # end
+  # 
+  # desc "Rollback a single commit."
+  # task :rollback, except: {no_release: true} do
+  #   set :branch, "HEAD^"
+  #   default
+  # end
+
   task :start do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec passenger start -d -p #{passenger_port}"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec passenger start -d -p #{passenger_port}"
   end
   
   task :stop do
-    run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec passenger stop -p #{passenger_port}"
+    run "cd #{current_path}; RAILS_ENV=#{rails_env} bundle exec passenger stop -p #{passenger_port}"
   end
   
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    stop
+    start
+    # run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
