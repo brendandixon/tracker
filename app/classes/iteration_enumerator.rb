fdec9607a70d0862331c9ff797b8dc839043de0e
Brendan Dixon <brendandixon@me.com>
2013-01-11 17:49:47 -0800

    - Changed IterationEnumerator to produce iteration slices

--- app/classes/iteration_enumerator.rb
+++ app/classes/iteration_enumerator.rb
@@ -7,30 +7,30 @@ class IterationEnumerator
 
   def each(initial_iteration = 0)
     if block_given?
-      self.each_with_iteration do |task, iteration|
-        yield task if task.present?
+      self.each_iteration do |iteration|
+        iteration.tasks.each {|task| yield task}
       end
     else
       Enumerator.new(self, :each, initial_iteration)
     end
   end
 
-  def each_with_iteration(initial_iteration = 0)
+  def each_iteration(initial_iteration = 0)
     if block_given?
       iteration = Iteration.new(@team, number: initial_iteration)
       
       @team.tasks.for_iteration(iteration.start_date).each do |task|
         
         until iteration.wants_task?(task)
-          yield nil, iteration
+          yield iteration
           iteration = Iteration.new(iteration)
         end
 
         iteration.add_task(task)
-        yield task, iteration
       end
+      yield iteration
     else
-      Enumerator.new(self, :each, initial_iteration)
+      Enumerator.new(self, :each_iteration, initial_iteration)
     end
   end
 
