5932fb4c736d5163ebe286f2aa10c78a7fb379c9
Brendan Dixon <brendandixon@me.com>
2013-02-05 20:55:14 -0800

    - Added development secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Tracker::Application.config.secret_token = ENV['TRACKER_SECRET']
+Tracker::Application.config.secret_token = begin
+                                              ENV['TRACKER_SECRET'] ||
+                                              '55118ba4dcd52576d0e243121967470b240776095eae006ba5d9dfee005eb52c1c975de5282f95f577805a4250edce9c1e78053531c0e65796f6e9ab0ac7488d'
+                                          end
