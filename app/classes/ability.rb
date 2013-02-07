b72e7ffc0cbd8f52553386052b31bbfb483fe78a
Brendan Dixon <brendandixon@me.com>
2013-02-07 10:37:30 -0800

    - Added additional permissions

--- app/classes/ability.rb
+++ app/classes/ability.rb
@@ -5,25 +5,26 @@ class Ability
     user ||= User.new
 
     if user.role?(:admin)
-      can :manage, [User]
+      can :manage, [Role, User]
+      can :read, [Category, Feature, Project, Story, Task, Team]
     end
 
     if user.role? :scrum_master
-      can :manage, [Project, Story, Task, Team]
+      can :manage, [Category, Feature, Project, Story, Task, Team]
       can :read, User
       can :edit, User, id: user.id
     end
 
     if user.role? :developer
-      can :read, [Project, Story, Task, Team, User]
+      can :read, [Category, Feature, Project, Story, Task, Team, User]
       can :create, [Story, Task]
       can :edit, [Story, Task]
       can :print, Task
       can :edit, User, id: user.id
     end
 
-    if user.roles? :observer
-      can :read, [Project, Story, Task, Team, User]
+    if user.role? :observer
+      can :read, [Category, Feature, Project, Story, Task, Team, User]
       can :print, Task
       can :edit, User, id: user.id
     end
